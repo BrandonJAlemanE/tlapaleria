1f1206f1233708a9ee6f71b409f5a86e358adb83
Brandon Aleman <brandonae11@outlook.com>
2025-01-18 00:00:15 -0600

    login

--- app/src/main/java/com/example/tlapa/MainActivity.java
+++ app/src/main/java/com/example/tlapa/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.tlapa;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnlogin;
+    Button Registro;
+    EditText Usuario;
+    EditText Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +30,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
 
-    public void RegistrarUsuario(View registro){
-        Intent miIntent = new Intent(this, Registrar.class);
-    }
+    btnlogin = findViewById(R.id.login_btn);
 
-    public void Home(View Home){
-        Intent miIntent = new Intent(this, Home.class);
+    btnlogin.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Usuario = findViewById(R.id.user_input);
+            String UsuarioS = Usuario.getText().toString();
+            Password = findViewById(R.id.password_input);
+            String PasswordS = Password.getText().toString();
+            if(UsuarioS == "Admin" && PasswordS == "aaaa"){
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Bienvenido", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Datos incorrectos", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
+
+    Registro = findViewById(R.id.Registrar_btn);
+    Registro.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, Registrar.class);
+            startActivity(intent);
+        }
+    });
     }
 }
--- app/src/main/java/com/example/tlapa/Registrar.java
+++ app/src/main/java/com/example/tlapa/Registrar.java
@@ -3,6 +3,7 @@ package com.example.tlapa;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,6 +15,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Registrar extends AppCompatActivity {
 
+    Button registro;
+    EditText nom;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,17 +28,16 @@ public class Registrar extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    EditText mensajetext;
-
-    public void RegistrarUsuario(View registro){
-        Intent miIntent = new Intent(this, Registrar.class);
-    }
-    public void Home(View Home){
-        mensajetext = findViewById(R.id.nombre_input);
-        String mensajeS = mensajetext.getText().toString();
-        Toast.makeText(Registrar.this,"Bienvenido" + mensajeS, Toast.LENGTH_SHORT).show();
-        Intent miIntent = new Intent(this, Home.class);
+        registro = findViewById(R.id.Registrar_btn);
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nom = findViewById(R.id.nombre_input);
+                String nom2 = nom.getText().toString();
+                Intent intent = new Intent(Registrar.this, Home.class);
+                startActivity(intent);
+                Toast.makeText(Registrar.this, "Bienvenido " + nom2, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
